3c59501157cc1afab0e0f631e3d250f397ff48b8
700014473 <700014473@2260I-L29-008>
2026-02-11 12:58:37 -0500

    Add project files.

---
+++ Topic 3.5 - Intro to Methods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_3._5___Intro_to_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
