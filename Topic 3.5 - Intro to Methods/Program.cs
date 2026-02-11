3d5d96cbb7ea042f7f687f2ad24d4a197457b973
700014473 <700014473@2260I-L29-008>
2026-02-11 13:17:53 -0500

    gtgt

--- Topic 3.5 - Intro to Methods/Program.cs
+++ Topic 3.5 - Intro to Methods/Program.cs
@@ -2,11 +2,55 @@
 {
     internal class Program
     {
+        public static void DrawCheck()
+        {
+            // void means it does not return any value. It just performs a task and then exits.
+            // DrawCheck is the name of the method.
+
+            Console.WriteLine("     /");
+            Console.WriteLine("    /");
+            Console.WriteLine("\\  /");
+            Console.WriteLine(" \\/ ");
+
+            // Backslash is an escape character, so to print a backslash, we need to use two backslashes.
+        }
+
+        public static void DrawX()
+        {
+            Console.WriteLine("\\  /");
+            Console.WriteLine(" \\/ ");
+            Console.WriteLine(" /\\");
+            Console.WriteLine("/  \\");
+            // Backslash is an escape character, so to print a backslash, we need to use two backslashes.
+        }
+
+        public static double CalculateTax(double price)
+        {
+            const 
+        }
+
         static void Main(string[] args)
         {
             Console.Title = "Intro to Methods";
+
+            /* 
+              Method is a re-usable block of code that performs a specific task. 
+              It can be called multiple times throughout the program.
+            */
+
+            /*
+              Use methods when:
+                1) Repeating code
+                2) Isolation of a block of code 
+            */
+
             Console.WriteLine("Hello, World!");
-            Console.Wirte
+            Console.WriteLine(" ");
+            DrawCheck();
+            Console.WriteLine(" ");
+            DrawX();
+
+
         }
     }
 }
